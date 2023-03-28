880e11753faa29898c3434c29c2548f3705625c8
Roy <pawit2540@hotmail.com>
2023-03-28 16:59:18 +0700

    export only module lib

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -2,21 +2,6 @@ import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import AgoraRTC__default from 'agora-rtc-sdk-ng';
 export * from 'agora-rtc-sdk-ng';
 
-function _objectWithoutPropertiesLoose(source, excluded) {
-  if (source == null) return {};
-  var target = {};
-  var sourceKeys = Object.keys(source);
-  var key, i;
-
-  for (i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i];
-    if (excluded.indexOf(key) >= 0) continue;
-    target[key] = source[key];
-  }
-
-  return target;
-}
-
 var AgoraRTCErrorCode;
 
 (function (AgoraRTCErrorCode) {
@@ -91,8 +76,8 @@ var AgoraRTCErrorCode;
   AgoraRTCErrorCode["METADATA_OUT_OF_RANGE"] = "METADATA_OUT_OF_RANGE";
 })(AgoraRTCErrorCode || (AgoraRTCErrorCode = {}));
 
-var createClient = function createClient(config) {
-  var client;
+const createClient = config => {
+  let client;
 
   function createClosure() {
     if (!client) {
@@ -102,315 +87,236 @@ var createClient = function createClient(config) {
     return client;
   }
 
-  return function () {
-    return createClosure();
-  };
+  return () => createClosure();
 };
 function createMicrophoneAndCameraTracks(audioConfig, videoConfig) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createMicrophoneAndCameraTracks(audioConfig, videoConfig)).then(function (_AgoraRTC$createMicro) {
-        tracks = _AgoraRTC$createMicro;
-        return tracks;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let tracks = null;
 
-  var tracks = null;
+  async function createClosure() {
+    tracks = await AgoraRTC__default.createMicrophoneAndCameraTracks(audioConfig, videoConfig);
+    return tracks;
+  }
+
   return function useMicrophoneAndCameraTracks() {
-    var _useState = useState(false),
-        ready = _useState[0],
-        setReady = _useState[1];
-
-    var _useState2 = useState(null),
-        agoraRTCError = _useState2[0],
-        setAgoraRTCError = _useState2[1];
-
-    var ref = useRef(tracks);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(tracks);
+    useEffect(() => {
       if (ref.current === null) {
-        createClosure().then(function (tracks) {
+        createClosure().then(tracks => {
           ref.current = tracks;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
         setReady(true);
       }
 
-      return function () {
+      return () => {
         tracks = null;
       };
     }, []);
     return {
-      ready: ready,
+      ready,
       tracks: ref.current,
       error: agoraRTCError
     };
   };
 }
 function createBufferSourceAudioTrack(config) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createBufferSourceAudioTrack(config)).then(function (_AgoraRTC$createBuffe) {
-        track = _AgoraRTC$createBuffe;
-        return track;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let track = null;
 
-  var track = null;
+  async function createClosure() {
+    track = await AgoraRTC__default.createBufferSourceAudioTrack(config);
+    return track;
+  }
+
   return function useBufferSourceAudioTrack() {
-    var _useState3 = useState(false),
-        ready = _useState3[0],
-        setReady = _useState3[1];
-
-    var _useState4 = useState(null),
-        agoraRTCError = _useState4[0],
-        setAgoraRTCError = _useState4[1];
-
-    var ref = useRef(track);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(track);
+    useEffect(() => {
       if (ref.current === null) {
-        createClosure().then(function (track) {
+        createClosure().then(track => {
           ref.current = track;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
         setReady(true);
       }
 
-      return function () {
+      return () => {
         track = null;
       };
     }, []);
     return {
-      ready: ready,
+      ready,
       track: ref.current,
       error: agoraRTCError
     };
   };
 }
 function createCameraVideoTrack(config) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createCameraVideoTrack(config)).then(function (_AgoraRTC$createCamer) {
-        track = _AgoraRTC$createCamer;
-        return track;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let track = null;
 
-  var track = null;
+  async function createClosure() {
+    track = await AgoraRTC__default.createCameraVideoTrack(config);
+    return track;
+  }
+
   return function useCameraVideoTrack() {
-    var _useState5 = useState(false),
-        ready = _useState5[0],
-        setReady = _useState5[1];
-
-    var _useState6 = useState(null),
-        agoraRTCError = _useState6[0],
-        setAgoraRTCError = _useState6[1];
-
-    var ref = useRef(track);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(track);
+    useEffect(() => {
       if (ref.current === null) {
-        createClosure().then(function (track) {
+        createClosure().then(track => {
           ref.current = track;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
         setReady(true);
       }
 
-      return function () {
+      return () => {
         track = null;
       };
     }, []);
     return {
-      ready: ready,
+      ready,
       track: ref.current,
       error: agoraRTCError
     };
   };
 }
 function createCustomAudioTrack(config) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createCustomAudioTrack(config)).then(function (_AgoraRTC$createCusto) {
-        track = _AgoraRTC$createCusto;
-        return track;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let track = null;
 
-  var track = null;
+  async function createClosure() {
+    track = await AgoraRTC__default.createCustomAudioTrack(config);
+    return track;
+  }
+
   return function useCustomAudioTrack() {
-    var _useState7 = useState(false),
-        ready = _useState7[0],
-        setReady = _useState7[1];
-
-    var _useState8 = useState(null),
-        agoraRTCError = _useState8[0],
-        setAgoraRTCError = _useState8[1];
-
-    var ref = useRef(track);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(track);
+    useEffect(() => {
       if (ref.current === null) {
-        createClosure().then(function (track) {
+        createClosure().then(track => {
           ref.current = track;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
         setReady(true);
       }
 
-      return function () {
+      return () => {
         track = null;
       };
     }, []);
     return {
-      ready: ready,
+      ready,
       track: ref.current,
       error: agoraRTCError
     };
   };
 }
 function createCustomVideoTrack(config) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createCustomVideoTrack(config)).then(function (_AgoraRTC$createCusto2) {
-        track = _AgoraRTC$createCusto2;
-        return track;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let track = null;
 
-  var track = null;
+  async function createClosure() {
+    track = await AgoraRTC__default.createCustomVideoTrack(config);
+    return track;
+  }
+
   return function useCustomVideoTrack() {
-    var _useState9 = useState(false),
-        ready = _useState9[0],
-        setReady = _useState9[1];
-
-    var _useState10 = useState(null),
-        agoraRTCError = _useState10[0],
-        setAgoraRTCError = _useState10[1];
-
-    var ref = useRef(track);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(track);
+    useEffect(() => {
       if (ref.current === null) {
-        createClosure().then(function (track) {
+        createClosure().then(track => {
           ref.current = track;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
         setReady(true);
       }
 
-      return function () {
+      return () => {
         track = null;
       };
     }, []);
     return {
-      ready: ready,
+      ready,
       track: ref.current,
       error: agoraRTCError
     };
   };
 }
 function createMicrophoneAudioTrack(config) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createMicrophoneAudioTrack(config)).then(function (_AgoraRTC$createMicro2) {
-        track = _AgoraRTC$createMicro2;
-        return track;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let track = null;
 
-  var track = null;
+  async function createClosure() {
+    track = await AgoraRTC__default.createMicrophoneAudioTrack(config);
+    return track;
+  }
+
   return function useMicrophoneAudioTrack() {
-    var _useState11 = useState(false),
-        ready = _useState11[0],
-        setReady = _useState11[1];
-
-    var _useState12 = useState(null),
-        agoraRTCError = _useState12[0],
-        setAgoraRTCError = _useState12[1];
-
-    var ref = useRef(track);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(track);
+    useEffect(() => {
       if (ref.current === null) {
-        createClosure().then(function (track) {
+        createClosure().then(track => {
           ref.current = track;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
         setReady(true);
       }
 
-      return function () {
+      return () => {
         track = null;
       };
     }, []);
     return {
-      ready: ready,
+      ready,
       track: ref.current,
       error: agoraRTCError
     };
   };
 }
 function createScreenVideoTrack(config, withAudio) {
-  var createClosure = function createClosure() {
-    try {
-      return Promise.resolve(AgoraRTC__default.createScreenVideoTrack(config, withAudio)).then(function (_AgoraRTC$createScree) {
-        tracks = _AgoraRTC$createScree;
-        return tracks;
-      });
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  let tracks;
 
-  var tracks;
+  async function createClosure() {
+    tracks = await AgoraRTC__default.createScreenVideoTrack(config, withAudio);
+    return tracks;
+  }
+
   return function useScreenVideoTrack() {
-    var _useState13 = useState(false),
-        ready = _useState13[0],
-        setReady = _useState13[1];
-
-    var _useState14 = useState(null),
-        agoraRTCError = _useState14[0],
-        setAgoraRTCError = _useState14[1];
-
-    var ref = useRef(tracks);
-    useEffect(function () {
+    const [ready, setReady] = useState(false);
+    const [agoraRTCError, setAgoraRTCError] = useState(null);
+    const ref = useRef(tracks);
+    useEffect(() => {
       if (ref.current === undefined) {
-        createClosure().then(function (tracks) {
+        createClosure().then(tracks => {
           ref.current = tracks;
           setReady(true);
-        }, function (e) {
+        }, e => {
           setAgoraRTCError(e);
         });
       } else {
@@ -418,22 +324,22 @@ function createScreenVideoTrack(config, withAudio) {
       }
     }, []);
     return {
-      ready: ready,
+      ready,
       tracks: ref.current,
       error: agoraRTCError
     };
   };
 }
-var AgoraVideoPlayer = function AgoraVideoPlayer(props) {
-  var vidDiv = useRef(null);
-
-  var videoTrack = props.videoTrack,
-      config = props.config,
-      other = _objectWithoutPropertiesLoose(props, ["videoTrack", "config"]);
-
-  useLayoutEffect(function () {
+const AgoraVideoPlayer = props => {
+  const vidDiv = useRef(null);
+  const {
+    videoTrack,
+    config,
+    ...other
+  } = props;
+  useLayoutEffect(() => {
     if (vidDiv.current !== null) videoTrack.play(vidDiv.current, config);
-    return function () {
+    return () => {
       videoTrack.stop();
     };
   }, [videoTrack]);
